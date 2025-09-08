f0e8789726123e631062aeff3087c6143b041c45
tgrozenski <tyler.grozenski@gmail.com>
2025-09-07 21:22:32 -0700

    added copy of frontend

---
+++ frontend/script.js
@@ -0,0 +1,6 @@
+const oAuthButton = document.getElementById('oAuth-btn');
+if (oAuthButton) {
+    oAuthButton.addEventListener('click', () => {
+        console.log('Google button clicked!');
+    });
+}
